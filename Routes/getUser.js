65bf56e05dbd3e034343c6ba8822579ee0858285
Borbot33 <58076174+Borbot33@users.noreply.github.com>
2024-03-16 17:53:35 +0000

    Update getUser.js

--- Routes/getUser.js
+++ Routes/getUser.js
@@ -16,7 +16,7 @@ export default {
                 if(response.status == 200) {
                     const user = response.data; const avatarNumber = user.discriminator === '0' ? getUserAvatar(user.id) : user.discriminator % 5;
                     res.send(`
-                        <link type="application/json+oembed" href="${process.env.domain}/emb/${response.data.id}.json" />
+                        <link type="application/json+oembed" href="${process.env.domain}${process.env.customPath}/emb/${response.data.id}.json" />
                         <meta content="${response.data.username}${response.data.discriminator == '0' ? "" : `#${response.data.discriminator}`}" property="og:title" />
                         <meta content="Created At: ${Math.floor(date / 1000)}\nAvatarHASH: ${user.avatar || "None"}\nAvatarNumber: ${avatarNumber}" property="og:description" />
                         <meta content="${process.env.domain}${process.env.customPath || ""}/api/userBanner/${req.params.userID}" property="og:image" />
@@ -26,7 +26,7 @@ export default {
                     `)
                 } else {
                     await res.send(`
-                        <link type="application/json+oembed" href="${process.env.domain}/emb/${response.data.id}.json" />
+                        <link type="application/json+oembed" href="${process.env.domain}${process.env.customPath}/emb/${response.data.id}.json" />
                         <meta content="Unknown Snowflake" property="og:title" />
                         <meta content="Created At: ${date.toLocaleString()}" property="og:description" />
                         <meta content="#43B581" data-react-helmet="true" name="theme-color" />
